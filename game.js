26df54423c7ec7c0d1f1b736f8eb59dc59bba238
Rodrigo Delgado <rod.delgado85@gmail.com>
2016-09-14 08:00:54 +1200

    dots ready

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -10,6 +10,7 @@ function start () {
 function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
+    dots[i].addEventListener('click', makeBlue)
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
   }
@@ -19,10 +20,17 @@ function makeGreen (evt) {
   evt.preventDefault()
   evt.target.classList.toggle('green')
   updateCounts()
+
+
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue (evt) {
 
+  evt.target.classList.toggle('blue')
+  updateCounts()
+
+}
 // CREATE FUNCTION hide HERE
 
 function updateCounts () {
@@ -31,7 +39,7 @@ function updateCounts () {
     green: 0,
     invisible: 0
   }
-  
+
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
   // Once you've done the counting, this function will update the display
